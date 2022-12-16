a6458a8335820be72cfe20ebbb82a0f9d2e621ee
vignesh k <vigneshk199825@.com>
2022-12-16 23:34:53 +0530

    day4task

--- Meals_deals/meal.js
+++ Meals_deals/meal.js
@@ -1,36 +1,144 @@
+async function fetchDocument() {
+    const result = await  fetch('./items.json')
+    const json = await result.json();
+    return json;
+}
+async function run() {
+  let products = await fetchDocument();
+  const itemDiv = document.getElementById("items");
+  for (let item of products) {
+    const div = document.createElement("div");
+    div.innerHTML += `
+            <div class="item">
 
+            <div style="background-position: 10%;">10%</div>
+                    
+                    <img src="${item.images}" alt="${item.images}">
+                    <div class = "tit">
+                        <h3>${item.title}</h3>
+                        
+                    </div>
+                    <p>${item.description}</p>
+                    <button class = 'my'>Get FREE Coupon</button>
 
-let http = new XMLHttpRequest();
+                    <div class="bottom">
 
-http.open('get', 'item.json',true);
+                    <div class="item_img">
+                        <img src="${item.full}" alt="${item.full}">
+                        <img src="${item.full1}" alt="${item.full1}">
+                        <img src="${item.full2}" alt="${item.full2}">
+                    </div>
 
+                    <div class="kind">
+                    <p>KindMeal Discount</p>
+                    <h2>10% off</h2>
+                    </div>
 
- http.send();
+                    <div class="off">
+                    <p>Expires In</p>
+                    <h2>6 Days</h2>
+                    </div>
 
- http.onload = function(){
-    if(this.readyState == 4 && this.status == 200){
-        let products = JSON.parse(this.responseText); 
+                    </div>
 
-        let output  = "";
-
-
-
-        for(let item of products){
-            output += `
-                <div class="item">
-                <img src="${item.images}" alt="${item.images}">
-                <p class="title">${item.title}</p>
-
-                <p class="description">${item.description}</p>
-                <p class="price">
-            <span></span>
-            <span>&euro;</span>
-            </p>
-                </div>
+            </div>
             `;
-        }
-        document.querySelector(".item").innerHTML=output;
-    }
- }
+    itemDiv.append(div);
+  }
+}
 
-  
+
+run();
+
+
+/********** search *********** */
+
+
+
+
+
+const search = document.getElementById("search-item")
+const matchlist = document.getElementById("items")
+
+
+
+async function searchItems(searchText){
+  const response = await fetch("items.json")
+  const co = await response.json()
+
+  let matches = co.filter(food => {
+    const regex = new RegExp(`^${searchText}`, "gi")
+    return food.title.match(regex)
+  })
+
+  if(search.value === ""){
+    matches = ""
+    matchlist.innerHTML =""
+  }
+  thehtml(matches)
+}
+
+
+function thehtml(products){
+  if(products.length > 0){
+    const itemDiv = document.getElementById("items");
+    itemDiv.innerHTML ="";
+  for (let item of products) {
+    const div = document.createElement("div");
+    div.innerHTML += `
+            <div class="item">
+
+            <div style="background-position: 10%;">10%</div>
+                    
+                    <img src="${item.images}" alt="${item.images}">
+                    <div class = "tit">
+                        <h3>${item.title}</h3>
+                        
+                    </div>
+                    <p>${item.description}</p>
+                    <button class = 'my'>Get FREE Coupon</button>
+
+                    <div class="bottom">
+
+                    <div class="item_img">
+                        <img src="${item.full}" alt="${item.full}">
+                        <img src="${item.full1}" alt="${item.full1}">
+                        <img src="${item.full2}" alt="${item.full2}">
+                    </div>
+
+                    <div class="kind">
+                    <p>KindMeal Discount</p>
+                    <h2>10% off</h2>
+                    </div>
+
+                    <div class="off">
+                    <p>Expires In</p>
+                    <h2>6 Days</h2>
+                    </div>
+
+                    </div>
+
+            </div>
+            `;
+    itemDiv.append(div);
+  }
+  }
+}
+search.addEventListener("input",() => searchItems(search.value))
+
+
+
+
+const page = document.getElementById("pages")
+const divPage = document.getElementById("items")
+
+
+
+async function searchPage(searchText){
+  const responses = await fetch("items.json")
+  const cot = await responses.json()
+
+ 
+
+  console.log(searchPage)
+}
